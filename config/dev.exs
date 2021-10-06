af2960be3a634b0d91bb2be0f8f68366b33e6c2c
Jacqui <jacquimanzi@gmail.com>
2021-10-06 10:50:30 -0700

    postgres setup

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :codebeam_2021, Codebeam2021.Repo,
   username: "postgres",
   password: "postgres",
-  database: "codebeam_2021_dev",
+  database: "postgres",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
